235892a9dec6ae3d61f174f0d25138e5b17a8a23
keith.hand <43491549+keithhand@users.noreply.github.com>
2024-06-20 17:41:40 -0500

    feat(sections): moves pure lua tables to .lua files

---
+++ latex/info/skills.lua
@@ -0,0 +1,24 @@
+SKILLS = {
+	["Technical Skills"] = {
+		"Python",
+		"Java",
+		"JavaScript",
+		"SQL",
+		"MongoDB",
+		"React",
+		"Angular",
+		"AWS",
+		"Azure",
+		"GCP",
+	},
+	["Soft Skills"] = {
+		"Communication (verbal and written)",
+		"Teamwork and collaboration",
+		"Problem-solving",
+		"Time management",
+	},
+	["Certifications"] = {
+		"Project Management Professional (PMP)",
+		"Cisco Certified Network Associate (CCNA)",
+	},
+}
